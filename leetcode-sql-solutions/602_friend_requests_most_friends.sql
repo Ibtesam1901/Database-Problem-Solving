356deff6addb8ac8b93da09e5010f73ff5736297
Syed Hassan Ibtesam <syedhassan7944@gmail.com>
2026-03-08 13:52:57 +0530

    Update 602_friend_requests_most_friends.sql

--- leetcode-sql-solutions/602_friend_requests_most_friends.sql
+++ leetcode-sql-solutions/602_friend_requests_most_friends.sql
@@ -1,1 +1,19 @@
-
+SELECT id, COUNT(*) AS num
+FROM (
+    SELECT requester_id AS id FROM RequestAccepted
+    UNION ALL
+    SELECT accepter_id AS id FROM RequestAccepted
+) t
+GROUP BY id
+HAVING num = (
+    SELECT MAX(cnt)
+    FROM (
+        SELECT COUNT(*) AS cnt
+        FROM (
+            SELECT requester_id AS id FROM RequestAccepted
+            UNION ALL
+            SELECT accepter_id AS id FROM RequestAccepted
+        ) x
+        GROUP BY id
+    ) y
+);
